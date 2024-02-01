9d87f6fd7c7581287fe2cea5427bcb46dd2450b5
aliumair0662 <aliumair0662@gmail.com>
2024-02-01 18:23:14 +0500

    daily visit  pdf template

--- resources/views/dailyVisits/dailyVisitPdf.blade.php
+++ resources/views/dailyVisits/dailyVisitPdf.blade.php
@@ -78,116 +78,59 @@
     {{-- <p class="text-right"><b>Sale Invoice</b></p> --}}
 
     <div class="card-body">
-        <h5 class="card-title">Add Visit Details</h5>
-        <div class="form-row">
-            <div class="col-md-2">
-                <div class="position-relative form-group">
-                    <label for="exampleEmail11" class="">Visit ID</label>
-                    <input name="invoice_number" id="invoice_number" placeholder="" type="text"
-                        value="{{ isset($dailyVisit) ? $dailyVisit->invoice_number : Config::get('constants.PACKAGE_INVOICE_PREFIX') . $invoice_number }}"
-                        class="form-control" readonly>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div class="position-relative form-group">
-                    <label for="exampleEmail11" class="">Current Date & Time</label>
-                    <input style="text-align:center;" name="invoice_date" id="invoice_date" placeholder=""
-                        type="text"
-                        value="{{ isset($dailyVisit) ? $dailyVisit->invoice_date : now()->format('d-m-Y h:i:s') }}"
-                        class="form-control" readonly>
-                </div>
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="col-md-6">
-                <div class="position-relative form-group">
-                    <label for="exampleEmail11" class="">Name</label>
-                    <input name="name" id="text" placeholder="Enter Name" type="text"
-                        value="{{ isset($dailyVisit) ? $dailyVisit->name : '' }}" class="form-control">
-                    <input name="id" id="id" value="{{ isset($dailyVisit) ? $dailyVisit->id : '' }}"
-                        type="hidden" class="form-control">
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="position-relative form-group">
-                    <label for="password" class="">Email</label>
-                    <input name="email" id="password" placeholder="Enter Email" type="email"
-                        value="{{ isset($dailyVisit) ? $dailyVisit->email : '' }}" class="form-control">
-                </div>
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="col-md-6">
-                <div class="position-relative form-group">
-                    <label for="phone" class="">Phone No</label>
-                    <input name="phone" id="phone" value="{{ isset($dailyVisit) ? $dailyVisit->phone : '' }}"
-                        type="text" class="form-control" placeholder="Enter Phone">
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="position-relative form-group">
-                    <label for="address" class="">Address</label>
-                    <input name="address" id="address" value="{{ isset($dailyVisit) ? $dailyVisit->address : '' }}"
-                        type="text" class="form-control" placeholder="Address">
-                </div>
-            </div>
-        </div>
-        {{-- d-none --}}
-        <div class="form-row">
-            <div class="col-md-6">
-                <div class="position-relative form-group">
-                    <label for="latitude" class="">Latitude</label>
-                    <input name="latitude" id="latitude"
-                        value="{{ isset($dailyVisit) ? $dailyVisit->latitude : '' }}" type="text"
-                        class="form-control" placeholder="Latitude" readonly>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="position-relative form-group">
-                    <label for="longitute" class="">Longitute</label>
-                    <input name="longitute" id="longitute"
-                        value="{{ isset($dailyVisit) ? $dailyVisit->longitute : '' }}" type="text"
-                        class="form-control" placeholder="Longitute" readonly>
-                </div>
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="col-md-12">
-                <div class="position-relative form-group">
-                    <label for="location" class="">Location</label>
-                    <input name="location" id="location"
-                        value="{{ isset($dailyVisit) ? $dailyVisit->location : '' }}" type="text"
-                        class="form-control" placeholder="Location" readonly>
-                </div>
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="col-md-6">
-                <label for="exampleEmail11" class="">Description</label>
-                <textarea name="description" id="description" placeholder="" type="text" value="" class="form-control">{{ isset($dailyVisit) ? $dailyVisit->description : '' }}</textarea>
-            </div>
-            <div class="col-md-3">
-                <div class="position-relative form-group">
-                    <label for="avatar" class="">Attachment</label>
-                    <input name="attachment" id="attachment" type="file" class="form-control-file"
-                        accept="image/*" capture="camera">
-                </div>
-            </div>
-            <div class="col-md-3">
-                @if (isset($dailyVisit))
-                    <div class="widget-content-left">
-                        <img width="100" class="" src="{{ asset($dailyVisit->attachment) }}"
-                            alt="">
-                    </div>
-                @endif
-            </div>
-        </div>
+        <h4 class="card-title" style="text-align: center">Visit Details</h4>
+        <table>
+            <tr>
+                <td>
+                    <p for="exampleEmail11"><b>Visit ID: </b>{{ $dailyVisit->invoice_number }}
+                    </p>
+                </td>
+                <td>
+                    <p for="exampleEmail11"><b>Current Date &
+                            Time:</b>{{ \Carbon\Carbon::parse($dailyVisit->invoice_date)->format('d-m-Y') }}</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p for="exampleEmail11"><b>Customer Name: </b>{{ $dailyVisit->name }}</p>
+                </td>
+                <td>
+                    <p for="password"><b>Email: </b>{{ $dailyVisit->email }}</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p for="phone"><b>Phone No: </b>{{ $dailyVisit->phone }}</p>
+                </td>
+                <td>
+                    <p for="address"><b>Address: </b>{{ $dailyVisit->address }}</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p for="latitude"><b>Latitude: </b>{{ $dailyVisit->latitude }}</p>
+                </td>
+                <td>
+                    <p for="longitute"><b>Longitute: </b>{{ $dailyVisit->longitute }}</p>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p for="location" class=""><b>Location:</b>{{ $dailyVisit->location }}</p>
+                </td>
+            </tr>
+        </table>
     </div>
 
 
-
+    <p class="mb-1"><b>Image Link:</b>{{ asset($dailyVisit->attachment) }} </p>
     <p class="mb-1"><b>Description:</b></p>
     <p class="mb-5">{{ $dailyVisit->description }}</p>
+    <hr>
+    <div class="mt-2" style="text-align: center;">
+        <img src="{{ public_path($dailyVisit->attachment) }}" alt="Image" width="350px">
+    </div>
+    {{-- {{ public_path($dailyVisit->attachment) }} --}}
     <div class="mt-5" style="position: fixed; bottom: 10%; left: 0; width: 100%;">
         <div class="d-inline-block">
             {{-- <p class="mb-0" style="font-size: 1.3rem; font-weight:900">Thank You for Your business!</p> --}}
